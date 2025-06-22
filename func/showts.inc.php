d3d835d9853f35dab0801f8e3eab6bd02cee3cdb
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-06-22 12:16:39 +0200

    Updating trainingsession view

--- func/showts.inc.php
+++ func/showts.inc.php
@@ -1,21 +1,11 @@
-<?php 
-    $trainingssessions = $_SESSION['trainingsessions'];
-    $length = count($trainingssessions);
-    $totaladded = 0;
-    if ($length <= 6) {
-        $cardnum = 1;
-        for($i = 0; $i < $length; $i++) {
-            $trainingses = $trainingssessions[$i][0];
-            $tsId = $trainingssessions[$i][1];
-            echo "<div class='card $cardnum'><a class='link-block' href='../app/planer.bp.php?id=$i&in=$tsId'>$trainingses</a></div>";
-            $totaladded++;
-            $cardnum++;
-        }
-        if ($totaladded < 6) {
-            for($j = 0; $totaladded < 6; $totaladded++) {
-                echo "<div class='card $cardnum'><a class='link-block' href='../includes/addts.inc.php'><i class='fas fa-plus-circle'></i></a></div>";
-                $cardnum++;
-            }
-        }
-    }
-?>
+<?php
+
+
+use CountFit\Models\TrainingSession;
+
+$trainingsSessions = TrainingSession::getCurrentUsersTrainingSessions();
+
+echo "<div class='card'><a class='link-block' href='../includes/addts.inc.php'><i class='fas fa-plus-circle'></i></a></div>";
+foreach ($trainingsSessions as $key => $trainingSession) {
+    echo "<div class='card'><a class='link-block' href='../app/planer.bp.php?id=$key&in=$trainingSession->tsId'>$trainingSession->tsName</a></div>";
+}
