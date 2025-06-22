ff42f96bf52925ad87575b94167879d9e2f0a017
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-06-22 12:15:21 +0200

    Updating include files

--- includes/addset.inc.php
+++ includes/addset.inc.php
@@ -1,30 +1,34 @@
-<?php 
+<?php
+
+use CountFit\Models\Exercise;
+
+require_once __DIR__ . '/../bootstrap.php';
+
 session_start();
 error_reporting(0);
-if (!isset($_SESSION['userId'])){
-    
+if (!isset($_SESSION['userId'])) {
     echo "user is not logged in";
     header("Location: ../login.php");
-}
-else {
-    if (!isset($_GET['id']) || !isset($_GET['exid'])) {
-        header("Location: ../app/planer.uebung.php?id=$exindex&mgs=150");
-    }
-    else {
-        session_start();
-        
-        if (isset($_POST['save'])) {
-            if(!isset($_POST['weight-input']) || !isset($_POST['reps-input'])) {
-                header("Location: ../app/planer.uebung.php?id=$exindex&msg=1550");
-            } 
-            else {
-                require_once '../func/save.inc.php';
-                header("Location: ../app/planer.uebung.php?id=$exindex&msg=4040");
-            }
+} else {
+
+    session_start();
+
+    if (isset($_POST['save'])) {
+        if (!isset($_POST['weight-input']) || !isset($_POST['reps-input'])) {
+            header("Location: ../app/planer.uebung.php?id=$exindex&msg=1550");
+        } else {
+            $exerciseId = $_GET['exid'];
+            $exercise = Exercise::getById($exerciseId);
+            $bpId = $_SESSION['currentBpId'];
+            $weight = floatval($_POST['weight-input']);
+
+            $reps = intval($_POST['reps-input']);
+
+            $set = $exercise->addSet($weight, $reps);
+            echo 'Set saved ' . $set . ' </br>';
+            header("Location: ../app/planer.uebung.php?id=$bpId&msg=4040");
         }
-        else {
-            header("Location: ../app/planer.uebung.php?id=$exindex");
-        }
+    } else {
+        header("Location: ../app/planer.uebung.php?id=$exindex");
     }
 }
-?>
