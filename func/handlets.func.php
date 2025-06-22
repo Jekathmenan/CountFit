c0b363c967b28d46dc1b2e962e8296145c4717ab
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-06-22 12:15:49 +0200

    Updating bodypart, exercise and trainingsession handler

--- func/handlets.func.php
+++ func/handlets.func.php
@@ -1,28 +1,13 @@
 <?php
 
-use CountFit\Models\TrainingSession;
+use CountFit\Controllers\TrainingSessionController;
 
 require_once __DIR__ . '/../bootstrap.php';
 
 session_start();
 
-/*error_reporting(E_ALL);
-error_reporting(-1);
-ini_set('error_reporting', E_ALL);*/
-
 if (!isset($_SESSION['userId'])) {
     header("Location: ../login.php");
 } else {
-    if (isset($_POST['ts-submit']) && isset($_POST['ts'])) {
-
-        // TODO: Inputvalidation
-        $tsname = mb_strtoupper($_POST['ts'], "UTF-8");
-
-        $trainingSession = new TrainingSession(tsName: $tsname);
-        $trainingSession->createTrainingSession();
-        header("Location: ../app/planer.main.php?msg=4050");
-    } else {
-        header("Location: ../includes/addts.inc.php?msg=3050");
-        exit();
-    }
+    TrainingSessionController::storeTrainingSession();
 }
