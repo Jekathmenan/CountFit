cf0c917514989f928ddb330bb1177fb1c679e759
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-06-22 12:16:47 +0200

    updating exercise view

--- func/showuebg.inc.php
+++ func/showuebg.inc.php
@@ -1,22 +1,17 @@
-<?php 
-    $exercises = $_SESSION['exercises'];
-    $length = count($exercises);
-    $totaladded = 0;
+<?php
 
-    if ($length <= 12) {
-        $cardnum = 1;
-        $exindex = $_GET['id'];
-        for($i = 0; $i < $length; $i++) {
-            $excerciseName = $exercises[$i][0];
-            $exid = $exercises[$i][1];
-            
-            echo "
-                <div class='card card$cardnum'>
+use CountFit\Models\Exercise;
+
+$exercises = Exercise::getAllRelevant();
+echo "<div class='card'><a class='link-block' href='../includes/addex.inc.php'><i class='fas fa-plus-circle'></i></a></div>";
+foreach ($exercises as $key => $ex) {
+    echo "
+                <div class='card'>
                     <div class='block'>
-                        $excerciseName
+                        " . $ex->getName() . "
                     </div>
                     <div class='input-exc'>
-                        <form class='exc' action='../includes/addset.inc.php?id=$exid&exid=$exindex' method='post'>
+                        <form class='exc' action='../includes/addset.inc.php?exid=" . $ex->getId() . "' method='post'>
                             <div class='weight'>
                                 <span class='label'> Gewicht in kg:</span>
                                 <div class='weight-adj'>
@@ -38,14 +33,4 @@
                     </div>
                 </div>
             ";
-            $cardnum++;
-            $totaladded++;
-        }
-        if ($totaladded < 12) {
-            for($j = 0; $totaladded < 12; $totaladded++) {
-                echo "<div class='card card$cardnum'><a class='link-block' href='../includes/addex.inc.php'><i class='fas fa-plus-circle'></i></a></div>";
-                $cardnum++;
-            }
-        }
-    }
-?>
+}
